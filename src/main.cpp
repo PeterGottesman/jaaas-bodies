e253b0c265c47797bced09edab59f5203a2f7ba5
Mike Leishear <mleishear23@gmail.com>
2021-11-07 06:42:22 -0500

    added test set of bodies to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,18 @@
 
 #include "app.hpp"
 #include "shader.hpp"
+#include "simulation.hpp"
 
 int main(int argc, char *argv[])
 {
+	struct *bodies testBodies;
+	bodies->x = {0,5}
+	bodies->y = {0,5}
+	bodies->z = {0,0}
+	bodies->mass = {50,10}
+	bodies->accelerations{ {0,0,0}, {0,0,0} }
+	bodies->velocities{ {0,0,0}, {2,3,0} }
+
 	App *a = App::get_instance();
 	a->run();
 }
